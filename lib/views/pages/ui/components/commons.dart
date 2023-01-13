fb649229ba49ebf000a0a5735e0a156a781072d7
Prajapati Chintan <prajapati.c001@webreinvent.com>
2023-01-13 16:04:03 +0530

    Added: Title styles and title selector

---
+++ lib/views/pages/ui/components/commons.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:team/vaahextendflutter/app_theme.dart';
+
+TextStyle heading = TextStyle(
+  fontSize: AppTheme.large,
+  fontWeight: FontWeight.bold,
+  color: AppTheme.colors['black']!.shade400,
+);
+
+TextStyle subheading = TextStyle(
+  fontSize: AppTheme.medium,
+  fontWeight: FontWeight.w600,
+  color: AppTheme.colors['black']!.shade400,
+);
+
+TextStyle normal = TextStyle(
+  fontSize: AppTheme.small,
+  fontWeight: FontWeight.w600,
+  color: AppTheme.colors['black']!.shade400,
+);
