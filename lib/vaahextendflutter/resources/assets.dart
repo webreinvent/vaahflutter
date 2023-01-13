245fe9058d9c3362b494717ad1f09e4b4f22846a
Prajapati Chintan <prajapati.c001@webreinvent.com>
2023-01-13 15:51:23 +0530

    Added: resources, Added: package flutter_svg

---
+++ lib/vaahextendflutter/resources/assets.dart
@@ -0,0 +1,23 @@
+class ImageAssets {
+  const ImageAssets._();
+
+  // General
+  static const iconGeneralBack = 'assets/icons/general/icon_back.svg';
+  static const iconGeneralCalendar = 'assets/icons/general/icon_calendar.svg';
+  static const iconGeneralClose = 'assets/icons/general/icon_close.svg';
+  static const iconGeneralDateTime = 'assets/icons/general/icon_datetime.svg';
+  static const iconGeneralDelete = 'assets/icons/general/icon_delete.svg';
+  static const iconGeneralError = 'assets/icons/general/icon_error.svg';
+  static const iconGeneralExpand = 'assets/icons/general/icon_expand.svg';
+  static const iconGeneralFile = 'assets/icons/general/icon_file.svg';
+  static const iconGeneralLocation = 'assets/icons/general/icon_location.svg';
+  static const iconGeneralNoConnection = 'assets/icons/general/icon_no_connection.svg';
+  static const iconGeneralPlayVideo = 'assets/icons/general/icon_play_video.svg';
+  static const iconGeneralRadioNormal = 'assets/icons/general/icon_radio_normal.svg';
+  static const iconGeneralRadioSelected = 'assets/icons/general/icon_radio_selected.svg';
+  static const iconGeneralRight = 'assets/icons/general/icon_right.svg';
+  static const iconGeneralSearch = 'assets/icons/general/icon_search.svg';
+  static const iconGeneralUser = 'assets/icons/general/icon_user.svg';
+  static const iconGeneralVideo = 'assets/icons/general/icon_video.svg';
+  static const iconGeneralWatch = 'assets/icons/general/icon_watch.svg';
+}
