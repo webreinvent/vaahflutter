917b0b03a738e6254022e84de966e0073e1125ec
Prajapati Chintan <prajapati.c001@webreinvent.com>
2023-01-13 14:11:33 +0530

    Added: package intl, Fixed: DateTime Helper

--- lib/vaahextendflutter/helpers/date_time.dart
+++ lib/vaahextendflutter/helpers/date_time.dart
@@ -1,229 +1,235 @@
-// import 'package:flutter/material.dart';
-// // import intl package
+import 'dart:math';
 
-// enum TimeZone { utc, local }
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:intl/intl.dart';
 
-// // Extension for DateTime
-// extension DateTimeExtension on DateTime {
-//   String toTime(BuildContext context) =>
-//       TimeOfDay.fromDateTime(asLocal()).format(context);
+enum TimeZone { utc, local }
 
-//   // Return DateTime with zero millisecond and microsecond
-//   DateTime resetMillisecond() {
-//     return DateTime(year, month, day, hour, minute, second);
-//   }
+extension TimeOfDayExtension on TimeOfDay {
+  // HH:MM AM
+  String toHmaa() {
+    return DateFormat('hh:mm aaaa').format(DateTime(0, 0, 0, hour, minute));
+  }
+}
 
-//   DateTime daysBefore(int days) => subtract(Duration(days: days));
+// Extension for DateTime
+extension DateTimeExtension on DateTime {
+  String toTime(BuildContext context) => TimeOfDay.fromDateTime(asLocal()).format(context);
 
-//   DateTime daysAfter(int days) => add(Duration(days: days));
+  // Return DateTime with zero millisecond and microsecond
+  DateTime resetMillisecond() {
+    return DateTime(year, month, day, hour, minute, second);
+  }
 
-//   DateTime nextDayStart() => onlyDate().daysAfter(1);
+  DateTime daysBefore(int days) => subtract(Duration(days: days));
 
-//   DateTime localTimeToday() => DateTime.now().let(
-//         (DateTime now) => DateTime(
-//           now.year,
-//           now.month,
-//           now.day,
-//           hour,
-//           minute,
-//           second,
-//           millisecond,
-//           microsecond,
-//         ),
-//       );
+  DateTime daysAfter(int days) => add(Duration(days: days));
 
-//   DateTime onlyDate() =>
-//       isUtc ? DateTime.utc(year, month, day) : DateTime(year, month, day);
+  DateTime nextDayStart() => onlyDate().daysAfter(1);
 
-//   DateTime onlyMonth() =>
-//       isUtc ? DateTime.utc(year, month) : DateTime(year, month);
+  DateTime localTimeToday() {
+    DateTime now = DateTime.now();
+    return DateTime(
+      now.year,
+      now.month,
+      now.day,
+      hour,
+      minute,
+      second,
+      millisecond,
+      microsecond,
+    );
+  }
 
-//   DateTime onlyTime([int? hourArg, int? minuteArg]) =>
-//       DateTime.utc(1970, 1, 1, hourArg ?? hour, minuteArg ?? minute, 0, 0, 0);
+  DateTime onlyDate() => isUtc ? DateTime.utc(year, month, day) : DateTime(year, month, day);
 
-//   DateTime utcTimeFirstDaySinceEpoch() =>
-//       DateTime.utc(1970, 1, 1, hour, minute, second, millisecond, microsecond);
+  DateTime onlyMonth() => isUtc ? DateTime.utc(year, month) : DateTime(year, month);
 
-//   // Convert local time as current utc
-//   // DateTime.now() = 2021-01-25 18:49:03.049422
-//   // DateTime.asUtc() = 2021-01-25 18:49:03.049422
-//   // DateTime.toUtc() = 2021-01-25 11:49:03.056208Z
-//   DateTime asUtc() => isUtc
-//       ? this
-//       : DateTime.utc(
-//           year,
-//           month,
-//           day,
-//           hour,
-//           minute,
-//           second,
-//           millisecond,
-//           microsecond,
-//         );
+  DateTime onlyTime([int? hourArg, int? minuteArg]) =>
+      DateTime.utc(1970, 1, 1, hourArg ?? hour, minuteArg ?? minute, 0, 0, 0);
 
-//   DateTime asLocal() => !isUtc
-//       ? this
-//       : DateTime(
-//           year,
-//           month,
-//           day,
-//           hour,
-//           minute,
-//           second,
-//           millisecond,
-//           microsecond,
-//         );
+  DateTime utcTimeFirstDaySinceEpoch() =>
+      DateTime.utc(1970, 1, 1, hour, minute, second, millisecond, microsecond);
 
-//   // Convert DateTime to String
-//   // Month(short) DD HH:MM AM/PM
-//   String toDateTimeString() {
-//     return DateFormat('MMM dd  h:mm a').format(this);
-//   }
+  // Convert local time as current utc
+  // DateTime.now() = 2021-01-25 18:49:03.049422
+  // DateTime.asUtc() = 2021-01-25 18:49:03.049422
+  // DateTime.toUtc() = 2021-01-25 11:49:03.056208Z
+  DateTime asUtc() => isUtc
+      ? this
+      : DateTime.utc(
+          year,
+          month,
+          day,
+          hour,
+          minute,
+          second,
+          millisecond,
+          microsecond,
+        );
 
-//   // Day, Month DD / HH:MM am/pm
-//   String toFullDateTimeString() {
-//     return DateFormat('EEEE, MMMM dd / h:mm a').format(this);
-//   }
+  DateTime asLocal() => !isUtc
+      ? this
+      : DateTime(
+          year,
+          month,
+          day,
+          hour,
+          minute,
+          second,
+          millisecond,
+          microsecond,
+        );
 
-//   // HH:MM AM
-//   String toHmaa() {
-//     return DateFormat('hh:mm aaaa').format(this);
-//   }
+  // Convert DateTime to String
+  // Month(short) DD HH:MM AM/PM
+  String toDateTimeString() {
+    return DateFormat('MMM dd  h:mm a').format(this);
+  }
 
-//   // HH:MM:SS
-//   String toHHMMSS() {
-//     return DateFormat('hh:mm:ss').format(this);
-//   }
+  // Day, Month DD / HH:MM am/pm
+  String toFullDateTimeString() {
+    return DateFormat('EEEE, MMMM dd / h:mm a').format(this);
+  }
 
-//   // Month DD
-//   String toMMMMdd() {
-//     return DateFormat('MMMM dd').format(this);
-//   }
+  // Day, Month DD am/pm
+  String toFullDateString() {
+    return DateFormat('EEE, yyyy MMMM dd').format(this);
+  }
 
-//   // Month(short) DD
-//   String toMMMdd() {
-//     return DateFormat('MMM dd').format(this);
-//   }
+  // HH:MM AM
+  String toHmaa() {
+    return DateFormat('hh:mm aaaa').format(this);
+  }
 
-//   // YYYY-MM-DD
-//   String toDateOfBirth() {
-//     return DateFormat('yyyy-MM-dd').format(this);
-//   }
+  // HH:MM:SS
+  String toHHMMSS() {
+    return DateFormat('hh:mm:ss').format(this);
+  }
 
-//   // To query format: YYYY-MM-DD
-//   String toQueryFormat() {
-//     return DateFormat('yyyy-MM-dd').format(this);
-//   }
-// }
+  // Month DD
+  String toMMMMdd() {
+    return DateFormat('MMMM dd').format(this);
+  }
 
-// // Extension for DateTime from String
-// extension DateTimeStringExtendsion on String {
-//   // Check Null or Empty
-//   bool get isNullOrEmpty => isNull || isEmpty;
+  // Month(short) DD
+  String toMMMdd() {
+    return DateFormat('MMM dd').format(this);
+  }
 
-//   // Convert to DateTime by pattern
-//   // https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
-//   DateTime toDateTime({String pattern = 'yyyy-MM-dd HH:mm:ss'}) {
-//     return isNullOrEmpty
-//         ? null
-//         : DateFormat(pattern).parse(this, true).toLocal();
-//   }
+  // YYYY-MM-DD
+  String toYyyymmmdd() {
+    return DateFormat('yyyy-MMM-dd').format(this);
+  }
 
-//   String safe([String supplier()]) =>
-//       this ?? (supplier == null ? '' : supplier());
+  // YYYY-MM-DD
+  String toDateOfBirth() {
+    return DateFormat('yyyy-MM-dd').format(this);
+  }
 
-//   String zeroPrefix(int count) {
-//     if (length >= count) {
-//       return this;
-//     } else {
-//       String builder = '';
-//       for (int i = 0; i < count - length; i++) {
-//         builder += '0';
-//       }
-//       builder += this;
-//       return builder;
-//     }
-//   }
+  // To query format: YYYY-MM-DD
+  String toQueryFormat() {
+    return DateFormat('yyyy-MM-dd').format(this);
+  }
+}
 
-//   int parseInt() => int.tryParse(this);
+// Extension for DateTime from String
+extension DateTimeStringExtendsion on String {
+  // Convert to DateTime by pattern
+  // https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+  DateTime toDateTime({String pattern = 'yyyy-MM-dd HH:mm:ss'}) {
+    return DateFormat(pattern).parse(this, true).toLocal();
+  }
 
-//   double parseDouble() => double.tryParse(this);
+  String safe() => this;
 
-//   String truncate(int limit) {
-//     return length > limit
-//         ? '${substring(0, min(length, limit)).trim()}...'
-//         : this;
-//   }
-// }
+  String zeroPrefix(int count) {
+    if (length >= count) {
+      return this;
+    } else {
+      String builder = '';
+      for (int i = 0; i < count - length; i++) {
+        builder += '0';
+      }
+      builder += this;
+      return builder;
+    }
+  }
 
-// // Extension for duration
-// extension DurationExtension on Duration {
-//   Duration safe([Duration supplier()]) =>
-//       this ?? (supplier == null ? Duration.zero : supplier());
+  int? parseInt() => int.tryParse(this);
 
-//   String format() {
-//     return toString().split('.').first.padLeft(8, '0');
-//   }
+  double? parseDouble() => double.tryParse(this);
 
-//   // Add zero padding
-//   String _twoDigits(int n) {
-//     if (n >= 10) {
-//       return '$n';
-//     }
-//     return '0${max(n, 0)}';
-//   }
+  String truncate(int limit) {
+    return length > limit ? '${substring(0, min(length, limit)).trim()}...' : this;
+  }
+}
 
-//   // hours:minutes:seconds
-//   String toHms() {
-//     final String twoDigitHours = _twoDigits(inHours.remainder(24) as int);
-//     final String twoDigitMinutes = _twoDigits(inMinutes.remainder(60) as int);
-//     final String twoDigitSeconds = _twoDigits(inSeconds.remainder(60) as int);
-//     return '$twoDigitHours:$twoDigitMinutes:$twoDigitSeconds';
-//   }
+// Extension for duration
+extension DurationExtension on Duration {
+  Duration safe() => this;
 
-//   // minutes:seconds
-//   String toMs() {
-//     final String twoDigitMinutes = _twoDigits(inMinutes.remainder(60) as int);
-//     final String twoDigitSeconds = _twoDigits(inSeconds.remainder(60) as int);
-//     return '$twoDigitMinutes:$twoDigitSeconds';
-//   }
-// }
+  String format() {
+    return toString().split('.').first.padLeft(8, '0');
+  }
 
-// // Extension for int
-// extension DateExtensions on int {
-//   DateTime localDateTime() =>
-//       DateTime.fromMillisecondsSinceEpoch(this, isUtc: false);
+  // Add zero padding
+  String _twoDigits(int n) {
+    if (n >= 10) {
+      return '$n';
+    }
+    return '0${max(n, 0)}';
+  }
 
-//   DateTime utcDateTime() =>
-//       DateTime.fromMillisecondsSinceEpoch(this, isUtc: true);
+  // hours:minutes:seconds
+  String toHms() {
+    final String twoDigitHours = _twoDigits(inHours.remainder(24));
+    final String twoDigitMinutes = _twoDigits(inMinutes.remainder(60));
+    final String twoDigitSeconds = _twoDigits(inSeconds.remainder(60));
+    return '$twoDigitHours:$twoDigitMinutes:$twoDigitSeconds';
+  }
 
-//   DateTime asDateTime({TimeZone from = TimeZone.utc}) {
-//     switch (from) {
-//       case TimeZone.local:
-//         return localDateTime();
-//       case TimeZone.utc:
-//       default:
-//         return utcDateTime();
-//     }
-//   }
+  // minutes:seconds
+  String toMs() {
+    final String twoDigitMinutes = _twoDigits(inMinutes.remainder(60));
+    final String twoDigitSeconds = _twoDigits(inSeconds.remainder(60));
+    return '$twoDigitMinutes:$twoDigitSeconds';
+  }
+}
 
-//   DateTime asLocal({TimeZone from = TimeZone.utc}) =>
-//       asDateTime(from: from).asLocal();
+// Extension for int
+extension DateExtensions on int {
+  DateTime localDateTime() => DateTime.fromMillisecondsSinceEpoch(this, isUtc: false);
 
-//   String toTime(BuildContext context, {TimeZone from = TimeZone.utc}) =>
-//       asDateTime(from: from).toTime(context);
+  DateTime utcDateTime() => DateTime.fromMillisecondsSinceEpoch(this, isUtc: true);
 
-//   int localTimeToday({TimeZone from = TimeZone.utc}) =>
-//       asDateTime(from: from).localTimeToday().millisecondsSinceEpoch;
+  DateTime asDateTime({TimeZone from = TimeZone.utc}) {
+    switch (from) {
+      case TimeZone.local:
+        return localDateTime();
+      case TimeZone.utc:
+      default:
+        return utcDateTime();
+    }
+  }
 
-//   int onlyDate({TimeZone from = TimeZone.utc}) =>
-//       asDateTime(from: from).onlyDate().millisecondsSinceEpoch;
+  DateTime asLocal({TimeZone from = TimeZone.utc}) => asDateTime(from: from).asLocal();
 
-//   int onlyTime({TimeZone from = TimeZone.utc}) =>
-//       asDateTime(from: from).utcTimeFirstDaySinceEpoch().millisecondsSinceEpoch;
+  String toTime(BuildContext context, {TimeZone from = TimeZone.utc}) =>
+      asDateTime(from: from).toTime(context);
 
-//   int utcTimeFirstDaySinceEpoch({TimeZone from = TimeZone.utc}) =>
-//       asDateTime(from: from).utcTimeFirstDaySinceEpoch().millisecondsSinceEpoch;
+  int localTimeToday({TimeZone from = TimeZone.utc}) =>
+      asDateTime(from: from).localTimeToday().millisecondsSinceEpoch;
 
-//   Duration asDuration() => Duration(milliseconds: this);
-// }
+  int onlyDate({TimeZone from = TimeZone.utc}) =>
+      asDateTime(from: from).onlyDate().millisecondsSinceEpoch;
+
+  int onlyTime({TimeZone from = TimeZone.utc}) =>
+      asDateTime(from: from).utcTimeFirstDaySinceEpoch().millisecondsSinceEpoch;
+
+  int utcTimeFirstDaySinceEpoch({TimeZone from = TimeZone.utc}) =>
+      asDateTime(from: from).utcTimeFirstDaySinceEpoch().millisecondsSinceEpoch;
+
+  Duration asDuration() => Duration(milliseconds: this);
+}
