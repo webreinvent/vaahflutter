c2b33483bc54d4c372140cb8583d81e0ea35463c
Mohd Imran <mohd-i001@webreinvent.com>
2024-05-23 12:17:39 +0530

    Updated: remodeled the Storage class
    - enforced type safety,
    - divided create, read and delete methods into two separate methods createAll, readAll, and deleteAll.
    - removed update method.
    - added precise documentation comments.

--- lib/vaahextendflutter/services/storage/storage.dart
+++ lib/vaahextendflutter/services/storage/storage.dart
@@ -10,20 +10,10 @@ abstract class Storage {
   ///The argument [name] is used to open Hive box with that name.
   ///
   ///If you don't provide [name], 'default' will be used.
-  ///
-  ///Usage:
-  ///```dart
-  ///import 'path/to/storage.dart';
-  /// //...
-  ///
-  /// final Storage storage = Storage.createLocal('users');
-  /// storage.create(key: 'john', value: 'John Doe');
-  ///
-  /// ```
-  factory Storage.createLocal({String? name}) {
+  factory Storage.createLocal({String name = 'default'}) {
     switch (_envConfig.localStorageType) {
       case LocalStorageType.hive:
-        final hive = HiveStorageImpl(name: name ?? 'default');
+        final hive = HiveStorageImpl(name: name);
         hive.init();
         return hive;
       case LocalStorageType.flutterSecureStorage:
@@ -33,76 +23,118 @@ abstract class Storage {
     }
   }
 
-  ///This methos is used to initialize the Storage.
-  ///Its not required in case of [FlutterSecureStorageImpl].
-  ///In case of [HiveStorageImpl] it creates a [Directory] using pat_provide package,
-  ///initializes hive at that directory, and opens a box with name [name] provided
-  ///during [Storage] creation.
+  ///Initializes the [Storage].
+  ///In the case of [HiveStorageImpl], it creates a [Directory] using the path_provide package,
+  ///initializes hive at that directory and opens a box with name [name] provided during [Storage]
+  ///creation.
+  ///It's not required in the case of [FlutterSecureStorageImpl].
+  /// example:
   ///```dart
   /// Storage.createLocal('name')
   /// ```
   Future<void> init();
 
-  ///Creates new item or items in the database.
+  ///Creates or updates new item in the [Storage].
   ///
-  ///It can take String or List<String> as [key], and the [value] can be of type,
-  ///String, List of String this String could be a Json String or a simple text accourding to your requirement.
-  ///
-  ///In case of Hive you can only provide the value as Map<String, String> where Map's key will be used as
-  ///[key] and Map's value will be used as [value].
-  ///
+  ///To save or update a single key-value pair pass [key] as String, and the [value] as String, the
+  ///String could be a JSON String or a simple text according to your requirement.
+  ///If the key is already present in the [Storage] it's vlaue will be overwritten.
   ///```dart
-  /// storage.create(key: 'john', value: 'John Doe');
+  ///await storage.create(key: 'key', value: 'value');
   /// ```
-  Future<void> create({dynamic key, dynamic value});
+  Future<void> create({required String key, required String value});
 
-  ///Reads the value of the item with [key] from the database and returns the value according to type of [key]
-  ///provided.
-  ///
-  ///It can take the [key] parameter as String and will return [value] as String.
-  ///If the [key] is List of String it will return [value] as List of String.
-  ///
-  ///When the key is not provided it will return all the values from that [Storage] as Map<[key], [value]>
+  ///Creates new items in the [Storage].
+  ///If you want to save multiple entries pass the [values] as a Map<String, String>, then it will
+  ///save all the key-value pairs in the [values] map.
+  ///If any key from the [values] is already present in the [Storage] it's value will be overwritten.
   ///```dart
-  /// storage.read(key: 'john');
+  ///await storage.createAll(values: {
+  ///   'key1': 'Value1',
+  ///   'key2': 'Value2',
+  ///   'key3': 'Value3',
+  ///   'key4': 'Value4',
+  ///   'key5': 'Value5',
+  ///   //...
+  ///   },
+  /// );
   /// ```
-  Future<dynamic> read({dynamic key});
+  Future<void> createAll({required Map<String, String> values});
 
-  ///Updates an item with the [key], and [value].
-  ///for data types refer [create].
+  ///Reads the value of the item at [key] from the [Storage] and returns the value.
+  ///
+  ///Read a single value by passing [key] as String, it will return the value as String?.
   ///```dart
-  /// storage.update(key: 'john', value: 'Changed value');
-  /// ```
-  Future<dynamic> update({dynamic key, dynamic value});
+  ///await storage.read(key: 'key');
+  ///```
+  Future<String?> read({required String key});
 
-  ///Deletes an item matching with the [key].
-  ///for data types refer [read].
+  ///Reads multiple values, pass the List of [keys] as argument. It will return the value as
+  ///Map<String, String?>.
+  ///
+  ///When the keys is not passed it will return all the values from that [Storage] as
+  ///Map<String, String?>
   ///```dart
-  /// storage.delete(key: 'john');
-  /// ```
-  void delete({dynamic key});
+  ///await storage.readAll(keys: [
+  ///   'key1',
+  ///   'key2',
+  ///   //...
+  ///  ],
+  ///);
+  ///```
+  Future<Map<String, String?>> readAll({List<String> keys = const []});
+
+  ///Deletes an item at [key].
+  ///```dart
+  ///await storage.delete(key: 'key');
+  ///```
+  Future<void> delete({required String key});
+
+  ///Deletes item at a key present in [keys], if keys is not passed all the values will be deleted
+  ///from that [Storage].
+  ///```dart
+  ///await storage.deleteAll(keys: [
+  ///   'key1',
+  ///   'key2',
+  ///   //...
+  ///  ],
+  ///);
+  ///```
+  Future<void> deleteAll({List<String> keys = const []});
 }
 
 ///A placeholder storage class when [LocalStorageType.none] is selected in env.dart.
 class NullStorage implements Storage {
   @override
-  void delete({dynamic key}) {}
-
-  @override
   Future<void> init() async {}
 
   @override
-  Future<String?> read({dynamic key}) {
+  Future<void> create({required String key, required String value}) {
     throw UnimplementedError();
   }
 
   @override
-  Future<String?> update({dynamic key, dynamic value}) {
+  Future<void> createAll({required Map<String, String> values}) {
     throw UnimplementedError();
   }
 
   @override
-  Future<void> create({dynamic key, dynamic value}) {
+  Future<String?> read({required String key}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Map<String, String?>> readAll({List<String> keys = const []}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> delete({required String key}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteAll({List<String> keys = const []}) {
     throw UnimplementedError();
   }
 }
