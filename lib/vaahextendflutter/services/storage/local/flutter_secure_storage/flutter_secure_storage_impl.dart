dd8b107933fa4f979969d160dfd81beb5b07cdcb
Mohd Imran <mohd-i001@webreinvent.com>
2024-05-23 12:53:59 +0530

    Updated: implemented the flutter secure storage

--- lib/vaahextendflutter/services/storage/local/flutter_secure_storage/flutter_secure_storage_impl.dart
+++ lib/vaahextendflutter/services/storage/local/flutter_secure_storage/flutter_secure_storage_impl.dart
@@ -3,68 +3,56 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../../storage.dart';
 
 class FlutterSecureStorageImpl implements Storage {
-  final storage = const FlutterSecureStorage();
+  final _storage = const FlutterSecureStorage();
+
   @override
   Future<void> init() async {}
 
   @override
-  Future<void> create({dynamic key, dynamic value}) async {
-    if ((key is List<String>) && (value is List<String>)) {
-      bool isKeyDataLengthEqual = (key.length == value.length);
-      if (isKeyDataLengthEqual) {
-        for (int i = 0; i < key.length; i++) {
-          await storage.write(key: key[i], value: value[i]);
-        }
-      }
-    } else if (key is String && value is String) {
-      await storage.write(key: key, value: value);
-    } else {
-      throw ArgumentError(
-          'key must be String or List<String>, data must be String, or List<String>');
+  Future<void> create({required String key, required String value}) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  @override
+  Future<void> createAll({required Map<String, String> values}) async {
+    for (String k in values.keys) {
+      await _storage.write(key: k, value: values[k]);
     }
   }
 
   @override
-  Future<dynamic> read({dynamic key}) async {
-    dynamic result;
-    if (key is String) {
-      result = await storage.read(key: key);
-      return result;
-    } else if (key is List<String>) {
-      result = List.empty(growable: true);
-      for (int i = 0; i < key.length; i++) {
-        result.add(await storage.read(key: key[i]));
-      }
-      return result;
-    } else if (key == null) {
-      result = await storage.readAll();
+  Future<String?> read({required String key}) async {
+    String? result = await _storage.read(key: key);
+    return result;
+  }
+
+  @override
+  Future<Map<String, String?>> readAll({List<String> keys = const []}) async {
+    if (keys.isEmpty) {
+      final Map<String, String> result = await _storage.readAll();
       return result;
     } else {
-      throw ArgumentError('key must be of type String or List<String>');
+      Map<String, String?> result = {};
+      for (String k in keys) {
+        result[k] = await _storage.read(key: k);
+      }
+      return result;
     }
   }
 
   @override
-  Future<dynamic> update({dynamic key, dynamic value}) {
-    create(key: key, value: value);
-    return read(key: key);
+  Future<void> delete({required String key}) async {
+    await _storage.delete(key: key);
   }
 
   @override
-  void delete({dynamic key}) async {
-    if (key is String) {
-      await storage.delete(key: key);
-    } else if (key is List<String>) {
-      for (int i = 0; i < key.length; i++) {
-        await storage.delete(key: key[i]);
-        storage.readAll();
-      }
-    } else if (key == null) {
-      await storage.deleteAll();
+  Future<void> deleteAll({List<String> keys = const []}) async {
+    if (keys.isEmpty) {
+      await _storage.deleteAll();
     } else {
-      throw ArgumentError(
-        'key must be of type String or List<String>',
-      );
+      for (String k in keys) {
+        await _storage.delete(key: k);
+      }
     }
   }
 }
