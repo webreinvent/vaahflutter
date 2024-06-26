d02b83dcfccc145e7195a7ec309e363cec657df6
Mohd Imran <mohd-i001@webreinvent.com>
2024-06-26 15:11:06 +0530

    Added: documentation comments createOrUpdate(Many), delete
    Added: comments deleteMany, deleteAll

--- lib/vaahextendflutter/services/storage/network/storage.dart
+++ lib/vaahextendflutter/services/storage/network/storage.dart
@@ -24,7 +24,7 @@ abstract class NetworkStorage {
   ///
   /// To create a single key-value pair, pass the [key] and the [value] as a Map<String, dynamic>.
   ///
-  /// Throws if the [key] is already present.
+  /// Throws if the [key] is already exists.
   /// Also throws if table [collectionName] does not exist in supabase.
   ///
   /// Example:
@@ -46,11 +46,12 @@ abstract class NetworkStorage {
     return _instanceNetwork.create(collectionName: collectionName, key: key, value: value);
   }
 
-  /// Creates new items in the [collectionName] of [NetworkStorage].
-  /// If you want to create multiple entries, pass the [values] as a Map<String, Map<String, dynamic>>,
-  /// then it will create all the key-value pairs from the [values] map.
+  /// Creates new items in the [collectionName].
+  /// If you want to create multiple entries, pass the [values] as a
+  /// Map<String, Map<String, dynamic>>, then it will create all the key-value pairs from the
+  /// [values] map.
   ///
-  /// Throws if any key from [values.keys] is already present.
+  /// Throws if any key from [values.keys] is already exists.
   /// Also throws if table [collectionName] does not exist in supabase.
   ///
   /// Example:
@@ -71,7 +72,7 @@ abstract class NetworkStorage {
     return _instanceNetwork.createMany(collectionName: collectionName, values: values);
   }
 
-  /// Reads the value of the item at [key] from the [NetworkStorage] and returns the value.
+  /// Reads the value of the item at [key] and returns the value.
   ///
   /// Read a single value by passing [key] as String, it will return the value as
   /// Map<String, dynamic>?.
@@ -90,8 +91,8 @@ abstract class NetworkStorage {
     return _instanceNetwork.read(collectionName: collectionName, key: key);
   }
 
-  /// Reads multiple values from [collectionName], pass the List of [keys] as an argument. It will
-  /// return the value as Map<String, Map<String, dynamic>?>.
+  /// Reads multiple values from [collectionName], pass the List of [keys]. It will return the
+  /// value as Map<String, Map<String, dynamic>?>.
   ///
   /// Example:
   /// ```dart
@@ -166,6 +167,19 @@ abstract class NetworkStorage {
     return _instanceNetwork.updateMany(collectionName: collectionName, values: values);
   }
 
+  /// Creates or updates an item in the [collectionName].
+  ///
+  /// To create or update a single key-value pair, pass the [key] and the [value]. If the [key] is
+  /// present it's value will be updated otherwise a new entry will be created.
+  ///
+  /// Example:
+  /// ```dart
+  /// NetworkStorage.createOrUpdate(
+  ///    collectionName: 'users-collection',
+  ///    key: 'sean',
+  ///    value: {'age': 34, 'email': 't@email.com'},
+  ///  ); // will get updated if the key 'sean' exists.
+  /// ```
   static Future<void> createOrUpdate({
     String collectionName = _vaahFlutterCollection,
     required String key,
@@ -174,6 +188,24 @@ abstract class NetworkStorage {
     return _instanceNetwork.createOrUpdate(collectionName: collectionName, key: key, value: value);
   }
 
+  /// Creates or updates multiple items in the [collectionName].
+  /// If you want to create or update multiple entries, pass the [values] as a
+  /// Map<String, Map<String, dynamic>>, then it will create all the key-value pairs from the
+  /// [values] map. If any key from the [values.keys] exits it's value will be updated otherwise a
+  /// new entry will be created.
+  ///
+  /// Example:
+  /// ```dart
+  ///  NetworkStorage.createOrUpdateMany(
+  ///    collectionName: 'users-collection',
+  ///    values: {
+  ///      'dave': {'name': 'Dave', 'age': 38, 'email': 'y@xz.com'},
+  ///      'sina': {'name': 'Sina', 'age': 32, 'email': 'y@xz.com'},
+  ///      'rock': {'name': 'Rock', 'age': 32, 'email': 'y@xz.com'},
+  ///      'sean': {'name': 'Sean'},
+  ///    },
+  ///  );
+  /// ```
   static Future<void> createOrUpdateMany({
     String collectionName = _vaahFlutterCollection,
     required Map<String, Map<String, dynamic>> values,
@@ -181,6 +213,12 @@ abstract class NetworkStorage {
     return _instanceNetwork.createOrUpdateMany(collectionName: collectionName, values: values);
   }
 
+  /// Deletes an item at [key] from [collectionName].
+  ///
+  /// Example:
+  /// ```dart
+  /// await NetworkStorage.delete(collectionName: 'users-collection', key: 'sean');
+  /// ```
   static Future<void> delete({
     String collectionName = _vaahFlutterCollection,
     required String key,
@@ -188,6 +226,15 @@ abstract class NetworkStorage {
     return _instanceNetwork.delete(collectionName: collectionName, key: key);
   }
 
+  /// Deletes items at keys present in [keys], from [collectionName].
+  ///
+  /// Example:
+  /// ```dart
+  /// await NetworkStorage.deleteMany(
+  ///    collectionName: 'users-collection',
+  ///     keys: ['dave', 'sean', 'sina'],
+  ///  );
+  /// ```
   static Future<void> deleteMany({
     String collectionName = _vaahFlutterCollection,
     required List<String> keys,
@@ -195,6 +242,12 @@ abstract class NetworkStorage {
     return _instanceNetwork.deleteMany(collectionName: collectionName, keys: keys);
   }
 
+  /// Deletes all the values from [collectionName].
+  ///
+  /// Example:
+  /// ```dart
+  ///  await NetworkStorage.deleteAll(collectionName: 'users-collection');
+  /// ```
   static Future<void> deleteAll({
     String collectionName = _vaahFlutterCollection,
   }) async {
